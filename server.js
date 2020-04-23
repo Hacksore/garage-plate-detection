c5a15359ef0031b0877323fe2031f80f97f42fd8
Sean Boult <hacksore@mcskinsearch.com>
2020-04-22 21:41:31 -0500

    Update data in the background not on get request

--- server.js
+++ server.js
@@ -3,23 +3,31 @@ const app = express();
 const { execSync } = require('child_process');
 const { CAM_USER, CAM_PASS, CAM_ADDRESS } = process.env;
 
+let detectedOutput = {
+  results: []
+};
+
 app.get('/', (req, res) => {
-  const response = JSON.parse(execSync('alpr /tmp/output.jpg -j').toString());
-  const detected = response.results.length > 0;
+  const results = detectedOutput.results
+  const detected = results.length > 0;
+  const plates = results.length > 0 ? results[0].candidates : [];
   
   return res.send({ 
     detected,
-    plates: response.results[0].candidates
+    plates
   });
 });
 
 const createImageFromStream = () => {
-  const command = `ffmpeg -y -i rtsp://${CAM_USER}:${CAM_PASS}@${CAM_ADDRESS} -vframes 1 /tmp/output.jpg > /dev/null`;
+  const command = `ffmpeg -hide_banner -loglevel panic -y -i rtsp://${CAM_USER}:${CAM_PASS}@${CAM_ADDRESS} -vframes 1 /tmp/output.jpg > /dev/null`;
   execSync(command);
+  detectedOutput = JSON.parse(execSync('alpr /tmp/output.jpg -j').toString());
 }
 
 // take image from stream
 createImageFromStream();
+
+// update every minute
 setInterval(() => createImageFromStream(), 1000 * 60);
 
 app.listen(8080, '0.0.0.0');
