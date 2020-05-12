5b1a169fd306bf9574fe7dc399e0919f0408066a
Sean Boult <hacksore@mcskinsearch.com>
2020-05-12 09:44:03 -0500

    Add build steps

--- README.md
+++ README.md
@@ -13,5 +13,5 @@ Use a network camera and raspberry pi to detect if vehicle is present in a garag
 # Running
 
 ```shell
-docker run -it -p 8080:8080 -e CAM_USER="user" -e CAM_PASS="pass" -e CAM_ADDRESS="adress" seanboult/gpd
+docker run -d -p 8080:8080 -e CAM_USER="user" -e CAM_PASS="pass" -e CAM_ADDRESS="adress" seanboult/gpd
 ```
--- server.js
+++ server.js
@@ -4,33 +4,45 @@ const { spawn } = require('child_process');
 const { CAM_USER, CAM_PASS, CAM_ADDRESS } = process.env;
 
 let detectedOutput = {
-  results: []
+  results: [],
 };
+let lastUpdate = null;
 
 app.get('/', (req, res) => {
-  const results = detectedOutput.results
+  const results = detectedOutput.results;
   const detected = results.length > 0;
   const plates = detected > 0 ? results[0].candidates : [];
-  
-  return res.send({ 
+
+  return res.send({
     detected,
     plates,
-    updated: Math.floor(new Date()/1000)
+    updated: lastUpdate,
   });
 });
 
 const createImageFromStream = () => {
-  const screenshotArgs = `-hide_banner -loglevel panic -y -i rtsp://${CAM_USER}:${CAM_PASS}@${CAM_ADDRESS} -vframes 1 /tmp/output.jpg`;
-  const screenshotCommand = spawn('ffmpeg', screenshotArgs.split(' '));
+  const screenshotCommand = spawn('ffmpeg', [
+    '-hide_banner',
+    '-loglevel',
+    'panic',
+    '-y',
+    '-i',
+    `rtsp://${CAM_USER}:${CAM_PASS}@${CAM_ADDRESS}`,
+    '-vframes',
+    '1',
+    '/tmp/output.jpg',
+  ]);
 
-  screenshotCommand.on('exit', (exitCode) => {
-    const handle = spawn('alpr', ['/tmp/output.jpg', '-j']);
-    handle.stdout.on('data', data => {
-      detectedOutput = JSON.parse(data.toString());
-    });
-  })  
+  screenshotCommand.on('exit', () => processImage());
+};
 
-}
+const processImage = () => {
+  const handle = spawn('alpr', ['/tmp/output.jpg', '-j']);
+  handle.stdout.on('data', (data) => {
+    detectedOutput = JSON.parse(data.toString());
+    lastUpdate = Math.floor(new Date() / 1000);
+  });
+};
 
 // take image from stream
 createImageFromStream();
