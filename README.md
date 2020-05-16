8afcf7e75393d458f28754908e844a88b176880a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2020-05-16 18:17:25 -0500

    Add exit code check

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Objective
 Use a network camera and raspberry pi to detect if vehicle is present in a garage.
 
-### Techonlogies 
+### Technologies 
 - docker
 - Wyze Cam V2 (Beta firmware for RTSP)
 - openalpr
@@ -15,3 +15,6 @@ Use a network camera and raspberry pi to detect if vehicle is present in a garag
 ```shell
 docker run -d -p 8080:8080 -e CAM_USER="user" -e CAM_PASS="pass" -e CAM_ADDRESS="adress" seanboult/gpd
 ```
+
+### Wyze Cam V2
+So the beta has a flaw that it goes unstable and you need to restart the device the device to make work again. I'm getting around this by using IFTTT to restart the camera every hour.
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const { spawn } = require('child_process');
+
 const { CAM_USER, CAM_PASS, CAM_ADDRESS } = process.env;
+const REFRESH_INTERVAL = process.env.REFRESH_INTERVAL || 60;
 
 let detectedOutput = {
   results: [],
@@ -33,7 +35,14 @@ const createImageFromStream = () => {
     '/tmp/output.jpg',
   ]);
 
-  screenshotCommand.on('exit', () => processImage());
+  screenshotCommand.on('exit', (exitCode) => {
+    // don't do anything if we can't get an image from the stream
+    if (exitCode > 0) {
+      return;
+    }
+
+    processImage();
+  });
 };
 
 const processImage = () => {
@@ -48,6 +57,6 @@ const processImage = () => {
 createImageFromStream();
 
 // update every minute
-setInterval(() => createImageFromStream(), 1000 * 60);
+setInterval(() => createImageFromStream(), 1000 * REFRESH_INTERVAL);
 
 app.listen(8080, '0.0.0.0');
